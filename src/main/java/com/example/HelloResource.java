1304787e3c1ef001c4a8b681a9b4e1454ffea765
Hiromitsu Iwata <hiromitsu7@gmail.com>
2022-07-17 12:44:30 +0900

    initial commit

---
+++ src/main/java/com/example/HelloResource.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/hello")
+public class HelloResource {
+
+  @GET
+  public String sayHello() {
+    return "Hello World";
+  }
+}

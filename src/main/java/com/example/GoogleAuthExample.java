baf947209858fef222414c8e6175c68b3144a71f
Hiromitsu Iwata <hiromitsu7@gmail.com>
2022-07-19 20:57:10 +0900

    画面表示追加

--- src/main/java/com/example/GoogleAuthExample.java
+++ src/main/java/com/example/GoogleAuthExample.java
@@ -13,14 +13,43 @@ import jakarta.inject.Named;
 @RequestScoped
 public class GoogleAuthExample {
 
+  private String otpAuthURL;
+  private String secret;
+
   public void authenticate() {
+
     GoogleAuthenticator gauth = new GoogleAuthenticator();
     final GoogleAuthenticatorKey key = gauth.createCredentials();
     final String secret = key.getKey();
     final List<Integer> scratchCodes = key.getScratchCodes();
+
     String otpAuthURL = GoogleAuthenticatorQRGenerator.getOtpAuthURL("Test Org.", "test@prova.org", key);
 
-    System.out.println("Please register (otpauth uri): " + otpAuthURL);
-    System.out.println("Secret key is " + secret);
+    for (Integer i : scratchCodes) {
+      System.out.println("Scratch code: " + i);
+    }
+
+    this.otpAuthURL = otpAuthURL;
+    this.secret = secret;
+
+    System.out.println("Please register (otpauth uri): " + this.otpAuthURL);
+    System.out.println("Secret key is " + this.secret);
   }
+
+  public String getOtpAuthURL() {
+    return otpAuthURL;
+  }
+
+  public String getSecret() {
+    return secret;
+  }
+
+  public void setOtpAuthURL(String otpAuthURL) {
+    this.otpAuthURL = otpAuthURL;
+  }
+
+  public void setSecret(String secret) {
+    this.secret = secret;
+  }
+
 }
